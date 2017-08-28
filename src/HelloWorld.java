b67af474b8dce4baf235b298e51c6ac787456eb4
artiom1997 <elena.kostiouk@gmail.com>
2017-08-28 09:46:49 -0400

    first attempt

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println(" HELLO everyine! "
+				+ "We can change everything here"
+				+ "and try to send it to git");
+		System.out.println("Lets try again");
+		
 	}
 
 }
